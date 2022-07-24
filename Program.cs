1fcd9d886977e1204b56ea20ef9538da6b70528d
Allen12900 <87824788+Allen12900@users.noreply.github.com>
2022-07-24 17:25:13 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace World_War_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to world war chess");
+        }
+    }
+}
