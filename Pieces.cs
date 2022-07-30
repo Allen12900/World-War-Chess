979a96c165eb4390374114df2d8cf4c37d6c95eb
DaBluNoodle <63121917+DaBluNoodle@users.noreply.github.com>
2022-07-30 12:31:44 +0100

    Created Pieces Class

---
+++ Pieces.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace World_War_Chess
+{
+    public class Pieces
+    {
+        public string name { get; set; }
+        public int health { get; set; }
+        public int attack {get; set; }
+        public int moveDistance { get; set; }
+        public int killDistace { get; set; }
+        public bool isWhite { get; set; }
+    }
+}
